e31d3fdb72bd2ca9458093614f40e1b6cd666c70
Sumukh M Lohit <43979295+sumukhmlohit@users.noreply.github.com>
2019-12-26 18:39:54 +0530

    Add files via upload

---
+++ pl_sql/trigger8.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TRIGGER tri_employee
+BEFORE insert or update
+ON employee
+FOR EACH ROW
+DECLARE
+rec varchar2(10) ;
+BEGIN
+SELECT to_char(sysdate ,'Dy') INTO rec FROM dual;
+IF rec = 'Thu' OR rec='Wed' THEN
+dbms_output.put_line( rec);
+raise_application_error (-20343, 'NOT ALLOWED TO ENTER');
+END IF ;
+END ;
+/
